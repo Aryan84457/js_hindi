adbce386a89385aa2208f53650070a50e28eb74c
Aryan84457 <144800499+Aryan84457@users.noreply.github.com>
2024-04-02 13:53:32 +0000

    datatype summary

---
+++ basics/datatypes.js
@@ -0,0 +1,21 @@
+//primitive
+// 7 types : string ,number ,Boolean , null, symbol,undefined,BigInt
+
+//javascript is an dynamic type language
+
+
+//Reference type(Non primitive)
+//array,objects ,functions
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
+const heros = ["shaktiman","naagraj","doga"];
+let myObj ={
+    name: "aryan",
+    age : 21,
+}
+const myFunction = function(){
+    console.log("Hello World");
+}
+console.log(typeof myFunction);
