62927879631ced489eeabf927d1eaf1abeede0e1
Aryan84457 <144800499+Aryan84457@users.noreply.github.com>
2024-04-02 13:35:42 +0000

    container config done

--- basics/test.js
+++ basics/test.js
@@ -2,8 +2,12 @@
 // let arya = 33;
 // arya="node";
 // console.log(arya);
-let accountbankName = "PNB";
-//let accountNumber = 808;
-let accountState = "Mandawar";
-let accountHolderName;
-console.table([accountbankName,accountState,accountHolderName])
+// let accountbankName = "PNB";
+// //let accountNumber = 808;
+// let accountState = "Mandawar";
+// let accountHolderName;
+// console.table([accountbankName,accountState,accountHolderName])
+
+ console.log("2"==2);
+ console.log(null==0);
+ console.log(undefined ==0);
