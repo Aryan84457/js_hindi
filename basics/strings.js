7cd21262c21c5b2ef1b6eb8bdc8f2cb016f5774c
Aryan84457 <144800499+Aryan84457@users.noreply.github.com>
2024-04-03 13:32:00 +0000

    done

---
+++ basics/strings.js
@@ -0,0 +1,36 @@
+// const name = "Aryan";
+// const repoCount = 50;
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+// const gameName = new String('hitesh-hc-coperation');
+// console.log(gameName);
+// const newStringOne = "  Aryan Agarwal ";
+// console.log(newStringOne.toUpperCase());
+// console.log(newStringOne.trim());
+// console.log(newStringOne.replace('a','n'));
+// console.log(newStringOne.split('a'));
+
+
+const score = 400;
+//console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 23.8966;
+// console.log(otherNumber.toPrecision(4));
+
+ const hundreds = 10000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+//+++++++++DATE+++++++++++++
+let myDate = new Date();
+// console.log(myDate.toLocaleString);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+
