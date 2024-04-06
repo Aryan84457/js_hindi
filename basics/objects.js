ac8680aef6b5bcd8a13788440986e3b4d5aba3ff
Aryan84457 <144800499+Aryan84457@users.noreply.github.com>
2024-04-06 10:15:28 +0000

    config done

---
+++ basics/objects.js
@@ -0,0 +1,49 @@
+const tinderUser = new Object();
+const sym = Symbol("key1")
+const jsUser = {
+    name: "Aryan",
+   [sym]: "mykey1",
+    email: "toshh@gmail.com",
+    "roll number": "arya 123",
+    lastLogin: ["M","W"]
+}
+
+
+// console.log(jsUser);
+// console.log(jsUser["roll number"]);
+// console.log(typeof jsUser["sym"]);
+jsUser.greeting = function(){
+    console.log("HELLO BIROO");
+}
+jsUser.greeting2= function(){
+    console.log(`HELLO BIROO ${this.name} Your Email id is : ${this.email}`);
+}
+console.log(jsUser.greeting());
+console.log(jsUser.greeting2());
+
+
+
+tinderUser.id = "rahul@gmail.com";
+tinderUser.name = "Rahul";
+tinderUser.islooggedIn = false;
+// console.log(tinderUser);
+
+const regularUser = {
+    userName :{
+        fullName:{
+            firstName : "Aryan ",
+            lastName : "Agarwal"
+        }
+    }
+}
+// console.log(regularUser.userName.fullName.lastName);
+
+const obj1 = { 1:"A",2:"B"};
+const obj2 = { 3:"C",4:"D"};
+const obj4 = { 3:"C",4:"D"};
+ const obj3 = Object.assign({},obj1,obj2,obj4)
+// console.log(obj3);
+// const obj5 = {...obj1,...obj2,...obj4};
+// console.log(obj5==obj3);
+
+   
