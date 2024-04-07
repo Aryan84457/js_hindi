0abe971abb755a20ba8fa6157b14252392fab5fd
Aryan84457 <144800499+Aryan84457@users.noreply.github.com>
2024-04-07 14:26:30 +0000

    done

---
+++ basics/control.js
@@ -0,0 +1,97 @@
+let isLoggedIn = true;
+
+// if(isLoggedIn){
+//     console.log("execute hogya bbhai , mai to");
+// }else{
+// console.log("Maaro mjhe");
+// }
+
+// const score = 200
+// if(score>100){
+//     const power = "fly"//var is not used due to scoope
+//     console.log(`user power : ${power}`);
+// }
+// console.log(`user power : ${power}`);
+
+// const balance =1000
+// // if(balance>500) console.log("test");
+
+// if(balance<500){
+//     console.log("less than 500");
+// }
+// else if(balance==1000){
+//   console.log("aree ye to 1000 k equal hai");
+
+// }
+
+// const userLoggedIn = true 
+// const debitCard = true 
+// if(userLoggedIn&& debitCard){
+//     console.log("kar le bhai jo karna hai tereko teri marzi");
+// }
+
+//const month = 3
+//  switch (month) {
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("Feb");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+//     case 4:
+//         console.log("April");
+//         break;
+//     case 5:
+//         console.log("JUnE");
+//         break;
+//     case 6:
+//         console.log("JULY");
+//         break;
+ 
+//     default:
+//         console.log("bbhai 12 hi mnths hote hai yrr");
+//         break;
+//  }
+
+//falsy value 
+/*
+false , 0 ,-0,BigInt 0n, "" , null  , undefined , NAN
+
+
+truthy values 
+"0" , 'false , " " , [] , {} , function(){}
+
+*/
+
+//Nullish coalescing Operator(??) : null undefined
+
+// let val1;
+// val1 = 5 ?? 10
+// val2 = null??10
+// val3 = undefined??15
+// console.log(val1);
+// console.log(val2);
+// console.log(val3);
+
+//Terniary operator
+
+
+//console.log(2<3? true: false)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
